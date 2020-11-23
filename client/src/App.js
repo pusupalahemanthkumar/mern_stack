779ff8604046fd930e8cd93fadf038a2adaa9181
hemanth kumar <pusupalahemanthkumar@gmail.com>
2020-11-23 13:15:58 +0530

    added header and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
+// Importing required files and packages here.
 import React from "react";
+import { Container } from "react-bootstrap";
 
-const App =()=>{
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+// Defining functional component here.
+const App = () => {
   return (
-    <div>
-      Welcome To Mern Stack
-    </div>
-  )
-}
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
 export default App;
